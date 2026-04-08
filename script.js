0449ff4207741e45d93db45cc920e249a8569d8d
corenitin <parmarnitin1306@gmail.com>
2026-04-09 02:04:45 +0530

    refactor: update HTML structure and improve JavaScript functionality for user profile display

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const githubApiUrl = "https://api.github.com/users/"
 const inputBar = document.querySelector("input")
 const searchBtn = document.querySelector("button")
+const userLink = document.getElementById("username-link")
 const avatar = document.querySelector("img")
 const userName = document.querySelector("h2")
 const nameOfuser = document.querySelector("#name")
@@ -15,14 +16,17 @@ async function findUser(user) {
         nameOfuser.textContent = "User not found"
     }
     let userData = await responce.json()
-
+    
+    userLink.href = userData.html_url
+    console.log(userLink.href);
+    
     avatar.src = userData.avatar_url
-    userName.textContent = `@${userData.login}`
-    nameOfuser.textContent = `Name: ${userData.name}`
-    bioOfuser.textContent = `Bio: ${userData.bio}`
-    followersOfUser.textContent = `Followers: ${userData.followers}`
-    followingOfUser.textContent = `Following: ${userData.following}`
-    repoOfUser.textContent = `Repositories: ${userData.public_repos}`
+    userName.textContent = `${userData.login}`
+    nameOfuser.textContent = `${userData.name}`
+    bioOfuser.textContent = `${userData.bio}`
+    followersOfUser.textContent = `${userData.followers}`
+    followingOfUser.textContent = `${userData.following}`
+    repoOfUser.textContent = `${userData.public_repos}`
     inputBar.value = ""
 }
 
