e34d1b74ad5c910c9b8638b0adbe7a84daf56f00
corenitin <parmarnitin1306@gmail.com>
2026-04-10 11:29:20 +0530

    fix: improve user search functionality and error handling

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const githubApiUrl = "https://api.github.com/users/"
 const title = document.querySelector("title")
 const inputBar = document.querySelector("input")
+const errorOfInput = document.getElementById("error-of-input")
 const searchBtn = document.querySelector("button")
 const userAcLink = document.getElementById("username-link")
 const avatar = document.querySelector("img")
@@ -12,32 +13,37 @@ const repoOfUser = document.querySelector("#repositories")
 const profileOfUser = document.querySelector("#profile-box")
 
 async function findUser(user) {
+    console.log(user);
+
     const responce = await fetch(githubApiUrl + user)
     if (responce.status == 404) {
-        nameOfuser.textContent = "User not found"
+        alert(`Please enter username`)
+
+    } else {
+        let userData = await responce.json()
+
+        title.textContent = userData.login
+
+        profileOfUser.classList.add("profile-box")
+
+        userAcLink.href = userData.html_url
+        userAcLink.textContent = `${userData.login}`
+
+        avatar.src = userData.avatar_url
+        nameOfuser.textContent = `${userData.name}`
+        bioOfuser.textContent = `${userData.bio}`
+
+        followersOfUser.href = `https://github.com/${user}?tab=followers`
+        followersOfUser.textContent = `Followers: ${userData.followers}`
+
+        followingOfUser.href = `https://github.com/${user}?tab=following`
+        followingOfUser.textContent = `Following: ${userData.following}`
+
+        repoOfUser.href = `https://github.com/${user}?tab=repositories`
+        repoOfUser.textContent = `Repositories: ${userData.public_repos}`
+        inputBar.value = ""
     }
-    let userData = await responce.json()
 
-    title.textContent = userData.login
-
-    profileOfUser.classList.add("profile-box")
-    
-    userAcLink.href = userData.html_url
-    userAcLink.textContent = `${userData.login}`
-    
-    avatar.src = userData.avatar_url
-    nameOfuser.textContent = `${userData.name}`
-    bioOfuser.textContent = `${userData.bio}`
-
-    followersOfUser.href = `https://github.com/${user}?tab=followers`
-    followersOfUser.textContent = `Followers: ${userData.followers}`
-
-    followingOfUser.href = `https://github.com/${user}?tab=following`
-    followingOfUser.textContent = `Following: ${userData.following}`
-
-    repoOfUser.href = `https://github.com/${user}?tab=repositories`
-    repoOfUser.textContent = `Repositories: ${userData.public_repos}`
-    inputBar.value = ""
 }
 
 inputBar.addEventListener('keydown', (e) => {
